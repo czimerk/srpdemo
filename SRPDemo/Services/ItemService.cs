2815ddd7e3d47e0d8a1f7d312bfb613638cd68fe
Czimer Kristóf <czimer.kristof@outlook.com>
2021-12-29 08:45:33 +0100

    adding access rights management

--- SRPDemo/Services/ItemService.cs
+++ SRPDemo/Services/ItemService.cs
@@ -8,45 +8,77 @@ namespace SRPDemo.Services
 {
     class ItemService
     {
-        public bool Create(Item item)
+        public bool Create(Item item, Guid userId)
         {
-            item.Id = Guid.NewGuid();
-            MemoryDb.Instance.Items.Add(item);
-            return true;
+            if (HasReadWriteAccess(userId))
+            {
+                item.Id = Guid.NewGuid();
+                MemoryDb.Instance.Items.Add(item);
+                return true;
+            }
+            return false;
         }
 
-        public bool Update(Item updatedItem)
+        public bool Update(Item updatedItem, Guid userId)
         {
-            var item = MemoryDb.Instance.Items.Find(i => i.Id == updatedItem.Id);
-            if (item == null)
-                return false;
+            if (HasReadWriteAccess(userId))
+            {
+                var item = MemoryDb.Instance.Items.Find(i => i.Id == updatedItem.Id);
+                if (item == null)
+                    return false;
 
-            item.Name = updatedItem.Name;
-            item.OwnerId = updatedItem.OwnerId;
-            item.Description = updatedItem.Description;
-            
-            return true;
+                item.Name = updatedItem.Name;
+                item.OwnerId = updatedItem.OwnerId;
+                item.Description = updatedItem.Description;
+
+                return true;
+            }
+            return false;
         }
 
-        public bool Delete(Guid itemId)
+        public bool Delete(Guid itemId, Guid userId)
         {
-            var item = MemoryDb.Instance.Items.Find(i => i.Id == itemId);
-            if (item == null)
-                return false;
+            if (HasReadWriteAccess(userId))
+            {
+                var item = MemoryDb.Instance.Items.Find(i => i.Id == itemId);
+                if (item == null)
+                    return false;
 
-            MemoryDb.Instance.Items.Remove(item);
-            return true;
+                MemoryDb.Instance.Items.Remove(item);
+                return true;
+            }
+            return false;
         }
 
-        public Item Get(Guid itemId)
+        public Item Get(Guid itemId, Guid userId)
         {
-            var item = MemoryDb.Instance.Items.Find(i => i.Id == itemId);
-            return item;
+            if (HasReadAccess(userId))
+            {
+                var item = MemoryDb.Instance.Items.Find(i => i.Id == itemId);
+                return item;
+            }
+
+            return null;
         }
 
-        public IEnumerable<Item> GetAll()
+        public IEnumerable<Item> GetAll(Guid userId)
         {
-            return MemoryDb.Instance.Items;
+            if (HasReadAccess(userId))
+                return MemoryDb.Instance.Items;
+
+            return null;
+        }
+
+        private bool HasReadAccess(Guid userId)
+        {
+            return MemoryDb.Instance.Roles.Find(r => r.UserId == userId
+            && (r.Access == AccessType.Read || r.Access == AccessType.ReadWrite)) != null;
+        }
+
+        private bool HasReadWriteAccess(Guid userId)
+        {
+            return MemoryDb.Instance.Roles.Find(r => r.UserId == userId
+            && r.Access == AccessType.ReadWrite) != null;
         }
     }
 }
